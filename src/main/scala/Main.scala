80c4d54ae9baae6174c98df7f1eec74280c3ab65
siddhartha-chandra <siddhartha.chandra0112@gmail.com>
2016-08-19 19:39:01 -0400

    fix: Converted hard-coded path to relative path

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,11 +10,12 @@ object Main{
   type Threshold = Double
   //as more than half the length for overlap is the criteria
   val threshold:Threshold = 0.5
-  //todo: fix path to relative
-  val filename = "/Users/siddharthachandra/Documents/programming/driver/ChromosomeBuilder/src/main/resources/data_set.txt"
+  val currentDirectory =  new java.io.File(".").getCanonicalPath
+  val fileName = "data_set.txt"
+  val filePath = s"$currentDirectory/src/main/resources/$fileName"
 
   def main(args: Array[String]): Unit = {
-    val lines = Source.fromFile(filename).getLines()
+    val lines = Source.fromFile(filePath).getLines()
     val DNASequences = Utils.getDNASequences(lines)
     val constructedChromosome = constructChromosome(DNASequences)
 
