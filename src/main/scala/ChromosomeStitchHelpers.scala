34f729338e5c32da1be63c57311a11211546f926
siddhartha-chandra <siddhartha.chandra0112@gmail.com>
2016-08-19 23:31:55 -0400

    replaced redundant function using hofs

--- src/main/scala/ChromosomeStitchHelpers.scala
+++ src/main/scala/ChromosomeStitchHelpers.scala
@@ -4,46 +4,7 @@ import scala.annotation.tailrec
 
 object ChromosomeStitchHelpers{
 
-  /**
-    *
-    * @param reference - This is the reference DNA sequence for which a right overlap needs to be found
-    * @param others - all other DNA sequences apart from the reference
-    * @return - right overlapped combined DNA Sequence
-    */
-
-  private def getRightOverlap(reference: DNASequence, others: List[DNASequence]) = {
-    def getNewAccumulator(reference: DNASequence, candidate: DNASequence, overlapLength: Int) = {
-      val newSequence = reference.sequence + candidate.sequence.drop(overlapLength)
-      val newFragment = reference.fragmentName + "|" +candidate.fragmentName
-      DNASequence(newFragment, newSequence)
-    }
-
-    def getRightOverlapInternal(rem: List[DNASequence],
-                                accumulator: DNASequence = DNASequence("","")): DNASequence = rem match {
-      case Nil => accumulator
-      case _ =>
-        //finds max right overlap text of accumulator and others
-        val overlapTextOpt = rem.map{o =>
-          accumulator.sequence.tails.find(o.sequence.startsWith)
-        }.maxBy(x => x.map(_.length)) //Assumption: that there should be only one maximum matching sequence
-
-        val candidateOpt = overlapTextOpt.flatMap(ot=> others.find(_.sequence.startsWith(ot)))
-
-        if (!isCandidateQualified(overlapTextOpt, candidateOpt))
-          getRightOverlapInternal(Nil, accumulator)
-        else {
-          val candidate = candidateOpt.get
-          val overlapText = overlapTextOpt.get
-          val newAccumulator = getNewAccumulator(accumulator, candidate, overlapText.length)
-          val filteredRem = rem.filterNot(_ == candidate)
-          getRightOverlapInternal(filteredRem, newAccumulator )
-        }
-    }
-    getRightOverlapInternal(others, reference)
-  }
-
-
-  def getDNASequenceForLeftOverlap(reference: DNASequence,
+  private def getDNASequenceForLeftOverlap(reference: DNASequence,
                                    candidate: DNASequence,
                                    overlapLength: Int) = {
     val newSequence = candidate.sequence.dropRight(overlapLength) + reference.sequence
@@ -51,7 +12,7 @@ object ChromosomeStitchHelpers{
     DNASequence(newFragment, newSequence)
   }
 
-  def getDNASequenceForRightOverlap(reference: DNASequence,
+  private def getDNASequenceForRightOverlap(reference: DNASequence,
                                     candidate: DNASequence,
                                     overlapLength: Int) = {
     val newSequence = reference.sequence + candidate.sequence.drop(overlapLength)
@@ -59,27 +20,25 @@ object ChromosomeStitchHelpers{
     DNASequence(newFragment, newSequence)
   }
 
-  def findMaxLeftOverlap(remainingDnaSequences: List[DNASequence],
-                         accumulator: DNASequence) = {
-    remainingDnaSequences.map{o =>
-      o.sequence.tails.find(accumulator.sequence.startsWith)
-    }.maxBy(x => x.map(_.length))
+  private def findLeftOverlap(remainingDnaSequences: List[DNASequence],
+                      accumulator: DNASequence) = {
+    remainingDnaSequences.map{r =>
+      r.sequence.tails.find(accumulator.sequence.startsWith)
+    }
   }
 
-  def findMaxRightOverlap(remainingDnaSequences: List[DNASequence],
-                         accumulator: DNASequence) = {
-    remainingDnaSequences.map{o =>
-      accumulator.sequence.tails.find(o.sequence.startsWith)
-    }.maxBy(x => x.map(_.length))
+  private def findRightOverlap(remainingDnaSequences: List[DNASequence],
+                       accumulator: DNASequence) = {
+    remainingDnaSequences.map{r =>
+      accumulator.sequence.tails.find(r.sequence.startsWith)
+    }
   }
 
-  def getCandidateOptLeftOverlap(overlapTextOpt: Option[String], remainingDNASequence: List[DNASequence]) = {
-    overlapTextOpt.flatMap(ot=> remainingDNASequence.find(_.sequence.endsWith(ot)))
-  }
+  private def getMatchingTextForLeftOverLap(dnaSequence: String, overlapText: String):Boolean =
+    dnaSequence.endsWith(overlapText)
 
-  def getCandidateOptRightOverlap(overlapTextOpt: Option[String], remainingDNASequence: List[DNASequence]) = {
-    overlapTextOpt.flatMap(ot=> remainingDNASequence.find(_.sequence.startsWith(ot)))
-  }
+  private def getMatchingTextForRightOverLap(dnaSequence: String, overlapText: String):Boolean =
+    dnaSequence.startsWith(overlapText)
 
   /**
     *
@@ -87,40 +46,36 @@ object ChromosomeStitchHelpers{
     * @param others - all other DNA sequences apart from the reference
     * @return - right overlapped combined DNA Sequence
     */
-  //todo: there is redundancy between the rightOverlap and leftOverlap methods. Would like to remove that using HOF
-  private def getLeftOverlap(reference: DNASequence, others: List[DNASequence]) = {
-    //todo: place 3 for removing redundancy
-    def getNewAccumulator(reference: DNASequence, candidate: DNASequence, overlapLength: Int) = {
-      val newSequence = candidate.sequence.dropRight(overlapLength) + reference.sequence
-      val newFragment = candidate.fragmentName + "|" + reference.fragmentName
-      DNASequence(newFragment, newSequence)
-    }
 
-    def getLeftOverlapInternal(rem: List[DNASequence],
+  private def getOverlap(reference: DNASequence,
+                             others: List[DNASequence],
+                             findOverlap: (List[DNASequence], DNASequence) => List[Option[String]],
+                             matchingTextForOverlap: (String, String) => Boolean,
+                             getDnaSequenceForAccumulator: (DNASequence, DNASequence, Int)=> DNASequence) = {
+
+    def getOverlapInternal(rem: List[DNASequence],
                                 accumulator: DNASequence = DNASequence("","")): DNASequence = rem match {
       case Nil => accumulator
       case _ =>
-        //finds max left overlap text of accumulator and others
 
-        //todo: place 1 where redundancy can be removed
-        val overlapTextOpt = rem.map{o =>
-          o.sequence.tails.find(accumulator.sequence.startsWith)
-        }.maxBy(x => x.map(_.length)) //Assumption: that there should be only one maximum matching sequence
+        //finds max overlap text of accumulator and remaining
+        val overlapTextOpt = findOverlap(rem, accumulator).
+          maxBy(x => x.map(_.length)) //Assumption: that there should be only one maximum matching sequence
 
-        //todo: place 2 where redundancy can be removed
-        val candidateOpt = overlapTextOpt.flatMap(ot=> others.find(_.sequence.endsWith(ot)))
+        val candidateOpt = overlapTextOpt.flatMap{ot=>
+          rem.find(c=> matchingTextForOverlap(c.sequence, ot))}
 
         if (!isCandidateQualified(overlapTextOpt, candidateOpt))
-          getLeftOverlapInternal(Nil, accumulator)
+          getOverlapInternal(Nil, accumulator)
         else {
           val candidate = candidateOpt.get
           val overlapText = overlapTextOpt.get
-          val newAccumulator = getNewAccumulator(accumulator, candidate, overlapText.length)
+          val newAccumulator = getDnaSequenceForAccumulator(accumulator, candidate, overlapText.length)
           val filteredRem = rem.filterNot(_ == candidate)
-          getLeftOverlapInternal(filteredRem, newAccumulator )
+          getOverlapInternal(filteredRem, newAccumulator )
         }
     }
-    getLeftOverlapInternal(others, reference)
+    getOverlapInternal(others, reference)
   }
 
   /**
@@ -131,10 +86,20 @@ object ChromosomeStitchHelpers{
   def constructChromosome(dnaSequences:List[DNASequence]) = dnaSequences match {
     case Nil => DNASequence("","")
     case h::t =>
-      val rightStitch = getRightOverlap(h,t)
+      val rightStitch = getOverlap(h,t,
+        findRightOverlap,
+        getMatchingTextForRightOverLap,
+        getDNASequenceForRightOverlap)
+
       val fragmentsInRightStitch = rightStitch.fragmentName.split('|')
       val availableFragments = dnaSequences.filterNot(x=> fragmentsInRightStitch.contains(x.fragmentName))
-      val builtChromosome = getLeftOverlap(rightStitch,availableFragments)
+
+      val builtChromosome = getOverlap(rightStitch,
+        availableFragments,
+        findLeftOverlap,
+        getMatchingTextForLeftOverLap,
+        getDNASequenceForLeftOverlap)
+
       val fragmentsInChromosome = builtChromosome.fragmentName.split('|')
       val unusedFragments = availableFragments.filterNot(x=> fragmentsInChromosome.contains(x.fragmentName))
 
